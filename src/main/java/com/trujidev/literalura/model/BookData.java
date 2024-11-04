acdfe1b57863ec41643a4fea461b4f1517aee9b0
TrujiDev <eetgrisales@gmail.com>
2024-11-04 18:55:36 -0500

    feat: define ResultData record for API response handling
    - Create ResultData record to encapsulate the list of BookData objects
    - Use @JsonIgnoreProperties to ignore unknown properties in JSON responses
    - Facilitate clean deserialization of API response data into Java objects

---
+++ src/main/java/com/trujidev/literalura/model/BookData.java
@@ -0,0 +1,15 @@
+package com.trujidev.literalura.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record BookData(
+    String title,
+    @JsonProperty("authors") List<AuthorData> author,
+    @JsonProperty("languages") String[] language,
+    int download_count
+) {
+}
