11cb0fadd499dbe603e6ddc0b26c9d34e2754da8
TrujiDev <eetgrisales@gmail.com>
2024-11-04 19:12:42 -0500

    feat: implement search functionality in Menu class
    - Added a menu interface for searching books by title.
    - Implemented method to fetch book data from external API.
    - Created logic to retrieve author details from fetched book data.
    - Added error handling for cases where no books are found based on the search title.
    - Enhanced user interaction with clear prompts and feedback.

--- src/main/java/com/trujidev/literalura/presentation/Menu.java
+++ src/main/java/com/trujidev/literalura/presentation/Menu.java
@@ -1,11 +1,15 @@
 package com.trujidev.literalura.presentation;
 
+import com.trujidev.literalura.model.Author;
 import com.trujidev.literalura.model.Book;
 import com.trujidev.literalura.model.BookData;
+import com.trujidev.literalura.model.ResultData;
+import com.trujidev.literalura.repository.BookRepository;
 import com.trujidev.literalura.service.ApiClient;
 import com.trujidev.literalura.service.JsonParser;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Scanner;
 
 public class Menu {
@@ -13,17 +17,24 @@ public class Menu {
   JsonParser jsonParser = new JsonParser();
   ApiClient apiClient = new ApiClient();
 
+  private final BookRepository repository;
   private final String url = "https://gutendex.com/books";
+
+  public Menu(BookRepository repository) {
+    this.repository = repository;
+  }
 
   public void showMenu() {
     var option = -1;
     while (option != 0) {
       var menu = """
           
+          *** Menu ***
           1 - Buscar por título
           
           0 - Salir
-          """;
+          
+          ¿Qué quieres hacer?:\s""";
       System.out.println(menu);
       option = sc.nextInt();
       sc.nextLine();
@@ -42,17 +53,31 @@ public class Menu {
     }
   }
 
-  private List<Book> fetchBookData(String title) {
+  private Optional<BookData> fetchBookData(String title) {
     var json = apiClient.fetchData(url + "/?search=" + title);
-    BookData bookData = jsonParser.fromJson(json, BookData.class);
-    return bookData.results();
+    ResultData resultData = jsonParser.fromJson(json, ResultData.class);
+    return resultData.results().stream().findFirst();
   }
 
   private void searchByTitle() {
     System.out.println("¿Qué título le interesa?: ");
     var title = sc.nextLine().replace(" ", "%20").toLowerCase();
-
     var result = fetchBookData(title);
-    System.out.println(result);
+
+    if (result.isPresent()) {
+      var data = result.get();
+
+      Author author = result.stream()
+          .flatMap(r -> r.author().stream()
+              .map(a -> new Author(a.name(), a.birth_year(), a.death_year())))
+          .findFirst()
+          .orElse(null);
+
+      Book book = new Book(data, author);
+      System.out.println(book);
+      repository.save(book);
+    } else {
+      System.out.println("No se encontraron resultados para el título: " + title);
+    }
   }
 }
