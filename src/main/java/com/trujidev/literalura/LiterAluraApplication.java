180eb79ac6712d85b1eccea700971ba65dc44d55
TrujiDev <eetgrisales@gmail.com>
2024-11-05 13:18:40 -0500

    feat: add search history and filter by language to Menu class
    - Added option to view search history by language (searchHistoryByLanguage).
    - Added full search history view (searchHistory), displaying all saved books.
    - Updated showMenu with new options for language-specific and full search history.
    - Enhanced book display with details: title, author, language, and download count.

--- src/main/java/com/trujidev/literalura/LiterAluraApplication.java
+++ src/main/java/com/trujidev/literalura/LiterAluraApplication.java
@@ -1,6 +1,8 @@
 package com.trujidev.literalura;
 
 import com.trujidev.literalura.presentation.Menu;
+import com.trujidev.literalura.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,13 +10,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LiterAluraApplication implements CommandLineRunner {
 
+	@Autowired
+	private BookRepository repository;
 	public static void main(String[] args) {
 		SpringApplication.run(LiterAluraApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		Menu menu = new Menu();
+		Menu menu = new Menu(repository);
 		menu.showMenu();
 	}
 }
