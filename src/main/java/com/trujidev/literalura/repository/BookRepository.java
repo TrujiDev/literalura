c3af14294f283150c4d4f1fb7b05153d4ff9f9e2
TrujiDev <eetgrisales@gmail.com>
2024-11-05 13:16:10 -0500

    feat: add method to find books by language in BookRepository
    - Implemented indByLanguage method in BookRepository to filter books based on language.
    - Enables retrieval of books matching a specified language attribute.

--- src/main/java/com/trujidev/literalura/repository/BookRepository.java
+++ src/main/java/com/trujidev/literalura/repository/BookRepository.java
@@ -3,4 +3,8 @@ package com.trujidev.literalura.repository;
 import com.trujidev.literalura.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BookRepository extends JpaRepository<Book, Long> { }
+import java.util.List;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+  List<Book> findByLanguage(String language);
+}
