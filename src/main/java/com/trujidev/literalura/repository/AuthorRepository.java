e398627c55781fc9403b92bfbdbd29cd3a02ec06
TrujiDev <eetgrisales@gmail.com>
2024-11-06 10:12:04 -0500

    fix: update query for living authors to include authors with no death date
    - Modify the `findByBirthYearLessThanEqualAndDeathYearGreaterThanEqual` method to include authors whose death date is `null`, indicating authors who are alive and have no recorded death date.
    - The query now considers authors born before or in the specified year and deceased after or in the specified year, or those without a death date.

--- src/main/java/com/trujidev/literalura/repository/AuthorRepository.java
+++ src/main/java/com/trujidev/literalura/repository/AuthorRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface AuthorRepository extends JpaRepository<Author, Long> {
-  List<Author> findByBirthYearLessThanEqualAndDeathYearGreaterThanEqual(int birthYear, int deathYear);
+  List<Author> findByBirthYearLessThanEqualAndDeathYearGreaterThanEqualOrDeathYearIsNull(int birthYear, int deathYear);
 }
