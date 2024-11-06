46edb5f6129cdee6e7aa5b7c5829938b9ba8c96c
TrujiDev <eetgrisales@gmail.com>
2024-11-06 10:13:18 -0500

    feat: add methods to count books by language and find distinct languages
    - Added `countByLanguage` to count the number of books for a specific language.
    - Added a custom query `findDistinctLanguages` to fetch a list of distinct languages available in the books.

--- src/main/java/com/trujidev/literalura/repository/BookRepository.java
+++ src/main/java/com/trujidev/literalura/repository/BookRepository.java
@@ -2,9 +2,14 @@ package com.trujidev.literalura.repository;
 
 import com.trujidev.literalura.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
   List<Book> findByLanguage(String language);
+  Long countByLanguage(String language);
+
+  @Query("SELECT DISTINCT b.language FROM Book b")
+  List<String> findDistinctLanguages();
 }
