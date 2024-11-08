6a08a8e7837f39e4ca518c26a0533f44b43fc7a4
TrujiDev <eetgrisales@gmail.com>
2024-11-08 10:37:45 -0500

    Add method to fetch top 10 books by download count in BookRepository
    - Added `findTop10ByOrderByDownloadCountDesc` method to retrieve the top 10 books ordered by the highest download count.
    - Ensured that the new method integrates with the existing repository structure.

--- src/main/java/com/trujidev/literalura/repository/BookRepository.java
+++ src/main/java/com/trujidev/literalura/repository/BookRepository.java
@@ -9,6 +9,7 @@ import java.util.List;
 public interface BookRepository extends JpaRepository<Book, Long> {
   List<Book> findByLanguage(String language);
   Long countByLanguage(String language);
+  List<Book> findTop10ByOrderByDownloadCountDesc();
 
   @Query("SELECT DISTINCT b.language FROM Book b")
   List<String> findDistinctLanguages();
